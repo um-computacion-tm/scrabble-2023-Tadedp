fe79757f483edef1866f39d782bfe751cf31f816
Tadedp <tadeodp22@gmail.com>
2023-09-10 20:37:20 -0300

    Added test for nextTurn method.

--- tests/testsScrabbleGame.py
+++ tests/testsScrabbleGame.py
@@ -9,5 +9,14 @@ class TestTile(unittest.TestCase):
         self.assertEqual(len(scrabbleGame.players[1].rack), 7)
         self.assertEqual(scrabbleGame.currentPlayer, None)
 
+    def testNextTurn(self):
+        scrabbleGame = ScrabbleGame(2) 
+        scrabbleGame.nextTurn()
+        self.assertEqual(scrabbleGame.currentPlayer, scrabbleGame.players[0])
+        scrabbleGame.nextTurn()
+        self.assertEqual(scrabbleGame.currentPlayer, scrabbleGame.players[1])
+        scrabbleGame.nextTurn()
+        self.assertEqual(scrabbleGame.currentPlayer, scrabbleGame.players[0])
+        
 if __name__ == '__main__':
     unittest.main()
