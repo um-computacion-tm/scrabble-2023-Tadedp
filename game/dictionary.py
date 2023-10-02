a85b0a78277cd42dbebb4f361b510ab681c51093
Tadedp <tadeodp22@gmail.com>
2023-10-01 23:13:54 -0300

    Added DictionaryConnectionFailed exception and changed log level of dle module.

--- game/dictionary.py
+++ game/dictionary.py
@@ -1,11 +1,16 @@
 from pyrae import dle
 
+class DictionaryConnectionFailedException(Exception):
+    pass
+
+dle.set_log_level(log_level = 'CRITICAL')
+
 class Dictionary:
     def isInDictionary(word: str):
         searchResult = dle.search_by_word(word)
-        failMessage = "Diccionario de la lengua española | Edición del Tricentenario | RAE - ASALE"
-        if searchResult.to_dict()["title"] == failMessage:
-            return False
-        else:
-            return True
+
+        if searchResult == None:
+            raise DictionaryConnectionFailedException("No fue ingresada una palabra o el servicio está caído.")
         
+        failMessage = "Diccionario de la lengua española | Edición del Tricentenario | RAE - ASALE"
+        return searchResult.title != failMessage
